0ba9206f671a47b5cf9410dc3a3846e35c821e29
Jakub Stóż <jakubstroz@gmail.com>
2024-03-02 00:37:22 +0100

    add category and producer to Product

--- Sklep/Produkty/admin.py
+++ Sklep/Produkty/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Producer, Category
 # Register your models here.
 admin.site.register(Product)
+admin.site.register(Producer)
+admin.site.register(Category)
--- Sklep/Produkty/models.py
+++ Sklep/Produkty/models.py
@@ -1,9 +1,33 @@
 from django.db import models
 
-class Product(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=100)
-    description = models.CharField(max_length=1000, blank=True)
-    price = models.IntegerField()
+
+    class Meta:
+        verbose_name = 'Kategoria'
+        verbose_name_plural = 'Kategorie'
 
     def __str__(self) -> str:
+        return self.name
+
+class Producer(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000, blank=True)
+
+    class Meta:
+        verbose_name = 'Producent'
+        verbose_name_plural = 'Producenci'
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+class Product(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,null=True,blank=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000, blank=True)
+    price = models.IntegerField()
+    producer = models.ForeignKey(Producer,on_delete=models.CASCADE,null=True)
+    def __str__(self) -> str:
         return f"id {self.id} - {self.name} - cena: {self.price/100}zł "
+    
