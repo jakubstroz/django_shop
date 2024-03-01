35e478e4b0aadf98006dca627b9c42dc0ad1eae7
Jakub Stóż <jakubstroz@gmail.com>
2024-03-01 23:58:28 +0100

    add product category and few examples

--- Sklep/Produkty/models.py
+++ Sklep/Produkty/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000, blank=True)
+    price = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"id {self.id} - {self.name} - cena: {self.price/100}zł "
